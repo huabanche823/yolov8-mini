9edf0124dfbeaeb29b0d8142502b367e4f14294f
huabanche <huabanche823@163.com>
2026-06-17 11:12:47 +0800

    WARP-train

--- mini-yolov8/examples/train.py
+++ mini-yolov8/examples/train.py
@@ -8,21 +8,27 @@ ROOT = Path(__file__).resolve().parents[1]
 
 def main():
     print(ROOT)
-    model = YOLO(ROOT / "yolov8n.pt") # 加载模型配置文件或权重文件，若没有权重文件，会自动下载预训练权重
+    # 加载模型配置文件或权重文件，若没有权重文件，会自动下载预训练权重
+    # 改进不要加载预训练权重
+    model = YOLO(ROOT / "/mnt/workspace/yolov8-mini/mini-yolov8/ultralytics/cfg/models/v8/yolov11.yaml") 
     results = model.train(
-        data=ROOT / "datasets/S2TLD/data.yaml",  # 数据集配置文件路径
+        data=ROOT / "datasets/data.yaml",  # 数据集配置文件路径
         epochs=100,  # 训练轮数
         imgsz=960,  # 输入图像大小
-        batch=8,  # 每次批处理的样本数
-        workers=0,  # 数据加载线程数，0 表示不使用子进程
-        device=0,  # 使用 CPU 进行训练
+        batch=32,   
+        lr0=0.001,  # 按缩放规则：0.01 * 32/16 = 0.02
+        momentum=0.95,
+        workers=4,  # 数据加载线程数，0 表示不使用子进程
+        device=0,  # 使用 GPU 进行训练
         project=ROOT / "runs",  # 训练结果保存目录的父目录
-        name="s2tld-yolov8n",  # 本次训练的实验名称
+        name="WARP-yolov8n",  # 本次训练的实验名称
         exist_ok=True,  # 如果目录已存在，允许覆盖
-        pretrained=True,  # 不使用预训练权重
-        val=True,  # 训练时不进行验证
-        plots=True,  # 不生成训练图表
-        verbose=True,  # 关闭详细日志输出
+        pretrained=False,  # 不使用预训练权重
+        val=True,  # 训练时进行验证
+        plots=True,  # 生成训练图表
+        verbose=True, # 关闭详细日志输出
+        amp=False,
+        optimizer="Adam"
     )
     print(f"train_ok task={model.task} save_dir={results.save_dir}")
 
