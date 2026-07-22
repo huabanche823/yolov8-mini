2b93dc7b6730dbd90657e2179373dc27bb68cc55
huabanche823 <huabanche823@163.com>
2026-07-22 09:30:51 +0800

    add result

---
+++ result.py
@@ -0,0 +1,40 @@
+'''
+Results module for the application.
+
+
+YOLOv11_grn4_repdown_neck summary (fused): 107 layers, 2,430,039 parameters, 9,600 gradients, 6.2 GFLOPs
+                 Class     Images  Instances      Box(P          R      mAP50  mAP50-95): 100% ━━━━━━━━━━━━ 6/6 2.3s/it 13.8s
+                   all        355       1894      0.938      0.846      0.921      0.662
+                 Brick        219        399      0.963      0.903      0.948       0.71
+              Concrete        217        522      0.983      0.916      0.986       0.77
+        Plastic Bottle        129        231      0.889      0.831      0.895       0.59
+         Reinforcement        174        312      0.932      0.704      0.842      0.556
+                timber        192        430      0.925      0.879      0.935      0.686
+Speed: 1.2ms preprocess, 4.4ms inference, 0.0ms loss, 6.6ms postprocess per image
+
+
+YOLOv11_grn4_d3c2f_p4p5_neck summary (fused): 106 layers, 2,419,831 parameters, 0 gradients, 6.1 GFLOPs
+                 Class     Images  Instances      Box(P          R      mAP50  mAP50-95): 100% ━━━━━━━━━━━━ 6/6 2.1it/s 2.8s
+                   all        355       1894      0.934      0.846      0.921      0.667
+                 Brick        219        399      0.942      0.897       0.95      0.706
+              Concrete        217        522      0.982      0.934      0.985       0.78
+        Plastic Bottle        129        231       0.89      0.827      0.896      0.593
+         Reinforcement        174        312      0.898      0.707      0.842      0.565
+                timber        192        430      0.956      0.865      0.932      0.691
+Speed: 1.5ms preprocess, 2.0ms inference, 0.0ms loss, 1.7ms postprocess per image
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
